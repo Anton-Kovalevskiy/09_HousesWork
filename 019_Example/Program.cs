a4900255caa6744b72db7bb9d650b16efcf728c9
Anton-Kovalevskiy <kaval1986@mail.ru>
2023-05-01 16:26:52 +0300

    Решение задачи №13 для любого числа

---
+++ 019_Example/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+// Решение задачи #19 для ЛЮБОГО введённого ЧИСЛА
+
+Console.Write("Введите число ->");
+string? number = Console.ReadLine();
+int i = number.Length;
+char[] reverseChar = new char[i];
+int k = 0;
+while (i > 0)
+{
+   i--;
+   reverseChar[i] = number[k];
+   k = k + 1;
+}
+string reverse = new string(reverseChar);
+Console.WriteLine($"Введённое число: {number}");
+Console.WriteLine($"Перевернутое число: {reverse}");
+if (number == reverse)
+{
+    Console.Write("Вывод: введённое число является палиндромом!");
+}
+else
+{
+    Console.Write("Вывод: введённое число не является палиндромом!");
+}
